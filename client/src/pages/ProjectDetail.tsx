9be7ae6f62d71e3a4d3a6fd61dad7ef664d9b2c5
Lokesh7812 <sudha09092002@gmail.com>
2025-11-18 00:47:36 +0530

    revision 8

--- client/src/pages/ProjectDetail.tsx
+++ client/src/pages/ProjectDetail.tsx
@@ -1117,7 +1117,7 @@ These achievers are living examples of resilience, empowerment, and opportunity.
       <section className="py-16 md:py-24 bg-background">
         <div className="container mx-auto px-4 sm:px-6 lg:px-8">
           <div className="max-w-4xl mx-auto">
-            <SectionHeader title="About This Program" />
+            <SectionHeader title="About This Initiatives" />
             <div className="mt-8">
               <FormattedDescription text={project.fullDescription} />
             </div>
